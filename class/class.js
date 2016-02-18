7fac01288eca8a3c937cd795d8658090faebe64c
zhoudd <zdd_910@126.com>
2016-02-18 22:32:32 +0800

    update

---
+++ class/class.js
@@ -0,0 +1,59 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+/**
+ * Created by zhoudd on 2016/2/17.
+ */
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "hello," + this.greeting;
+    };
+    return Greeter;
+})();
+var greeter = new Greeter('world');
+//继承
+var Animal = (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    Animal.prototype.move = function (meters) {
+        if (meters === void 0) { meters = 0; }
+        alert(this.name + ' moved ' + meters + "m.");
+    };
+    return Animal;
+})();
+var Snake = (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        _super.call(this, name);
+    }
+    Snake.prototype.move = function (meters) {
+        if (meters === void 0) { meters = 5; }
+        alert('Slithering...');
+        _super.prototype.move.call(this, meters);
+    };
+    return Snake;
+})(Animal);
+var Horse = (function (_super) {
+    __extends(Horse, _super);
+    function Horse(name) {
+        _super.call(this, name);
+    }
+    Horse.prototype.move = function (meters) {
+        if (meters === void 0) { meters = 45; }
+        alert('Galloping...');
+        _super.prototype.move.call(this, meters);
+    };
+    return Horse;
+})(Animal);
+var sam = new Snake('Sammy the python');
+var tom = new Horse('Tommy the Palomion');
+sam.move();
+tom.move(34);
+//# sourceMappingURL=class.js.map
