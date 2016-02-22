570131dc56b676aaf39bc07d18013b2fd6e9ee68
zhoudd <zdd_910@126.com>
2016-02-22 23:08:35 +0800

    update server

--- example/server/app.js
+++ example/server/app.js
@@ -3,27 +3,30 @@ var bodyParser = require('body-parser');
 /**
  * Registration
  */
-class Registration {
-    constructor(registration) {
+var Registration = (function () {
+    function Registration(registration) {
         this.salutation = registration.salutation;
         this.name = registration.name;
         this.age = registration.age;
     }
-    isValid() {
+    Registration.prototype.isValid = function () {
         return this.age >= 18;
-    }
-}
+    };
+    return Registration;
+})();
 var registrations = new Array();
 registrations.push({ salutation: 'zhou', name: 'dongdong', age: 12 }, { salutation: 'zhou1', name: 'dongdong', age: 12 });
 var app = express();
 app.use(bodyParser());
 app.use(express.static('../client'));
-app.get('/api/registrations', (req, res) => {
+app.get('/api/registrations', function (req, res) {
+    console.log(registrations);
     res.send(registrations);
 });
-app.post('/api/register', (req, res) => {
+app.post('/api/register', function (req, res) {
     var registration = new Registration(req.body);
     if (registration.isValid()) {
+        console.log(registration);
         registrations.push(registration);
         res.send(201);
     }
@@ -31,5 +34,12 @@ app.post('/api/register', (req, res) => {
         res.send(400);
     }
 });
-app.listen(process.env.PORT || 3000);
+app.use(function (req, res, next) {
+    console.log('Time', Date.now());
+    console.log(req, res);
+    next();
+});
+app.listen(process.env.PORT || 3000, function () {
+    console.log('listening.... port 3000');
+});
 //# sourceMappingURL=app.js.map
