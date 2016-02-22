5fc5b90440af4d63dc40ddb26ef792a1b0381a79
zhoudd <zdd_910@126.com>
2016-02-22 22:32:11 +0800

    整理目录

--- modules/Validation.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ID on 16/2/19.
- * Author:zhoudd
- * email:zhoudd@stark.tm
- */
-export interface StringValidator {
-    isAcceptable(s:string):boolean;
-}
---
+++ study/modules/Validation.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by ID on 16/2/19.
+ * Author:zhoudd
+ * email:zhoudd@stark.tm
+ */
+export interface StringValidator {
+    isAcceptable(s:string):boolean;
+}
